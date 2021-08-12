76c674f4a9936ca9701141f2ac1018031f8243be
aliakgul <devaliakgul@gmail.com>
2021-08-13 01:43:58 +0300

    first commit

---
+++ 2D Platformer/Assets/Scripts/EnemyBlock.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBlock : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    public Transform endPoint, startPoint;
+    private bool isFalling;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPoint.parent = null;
+        endPoint.parent = null;
+        isFalling = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFalling)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.position, moveSpeed * Time.deltaTime);
+            if(transform.position == endPoint.position)
+            {
+                isFalling = false;
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPoint.position, (moveSpeed / 3) * Time.deltaTime);
+            if (transform.position == endPoint.position)
+            {
+                isFalling = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isFalling = true;
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# A showcase repo of simple 2d games developed with Unity.
+
+> ❗ **2020-07-27 Since this is a showcase repo, not all assets are included.**
+
+### Videos
+[![2d_platformer](images/2d_platformer.png "2d_platformer")](https://youtu.be/btOJg3OQC3o)
+[![2d_rpg](images/2d_rpg.png "2d_rpg")](https://youtu.be/6coezQvTLqE)
+
+### Resources
+    https://www.udemy.com/course/unityplatformer/
+    https://www.udemy.com/course/unity2drpg/
